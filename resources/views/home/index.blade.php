5398682cf872f13b7f381ea2efb2f18136ac4f61
Luka Gogoladze <gogoladzeluk@gmail.com>
2022-06-07 14:04:32 +0400

    change landing page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,18 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container" style="position: absolute;top: 10%;bottom: 0px;left:0px;right:0px;z-index: -1;">
-        <img src="{{ asset('files/landing_page_background.jpg') }}" style="width: 100%; margin-top: -50px;">
-        <a href="{{ route('mechanics.index') }}" class="btn btn-warning" style="
-            position: absolute;
-            left: 6%;
-            bottom: 5%;
-            color: white;
-            padding: 5px 20px;
-            font-size: 35px;
-            font-weight: bold;
-            border-radius: 25px;
-            z-index: 1">
-            {{ __('Search') }}</a>
+    <div class="landing-image-landscape" style="width: 100%; height: 100%; margin-top: -70px;">
+        <a href="{{ route('mechanics.index') }}">
+            <img src="{{ asset('files/landing_landscape.png') }}" style="width: 100%;">
+        </a>
+    </div>
+    <div class="landing-image-portrait" style="width: 100%; height: 100%; margin-top: -50px;">
+        <a href="{{ route('mechanics.index') }}">
+            <img src="{{ asset('files/landing_portrait.png') }}" style="width: 100%;">
+        </a>
     </div>
 @endsection
