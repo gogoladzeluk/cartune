d1772a3b418ab590b86f149ad6399422ddd1a1de
Luka Gogoladze <gogoladzeluk@gmail.com>
2022-06-07 16:24:05 +0400

    use bootstrap footer everywhere

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -92,14 +93,19 @@
             @yield('content')
         </main>
 
-        <div class="footer">
-            <div class="footer-container">
-                <div class="footer-center">
-                    <a href="https://www.facebook.com/CarTuuune/" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a>
-                    <a href="https://www.linkedin.com/company/cartune0/" target="_blank"><i class="fa fa-linkedin-square fa-2x"></i></a>
-                </div>
+        <footer class="d-flex flex-wrap justify-content-start align-items-center mt-auto py-3 border-top">
+            <div class="col-md-4 d-flex align-items-center">
+                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                    <svg class="bi" width="30" height="24"></svg>
+                </a>
+                <span class="text-muted">© {{ now()->year }} CarTune, Inc</span>
             </div>
-        </div>
+
+            <ul class="nav col-md-4 justify-content-center list-unstyled d-flex">
+                <li class="ms-3"><a class="text-muted" href="https://www.facebook.com/CarTuuune/" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+                <li class="ms-3"><a class="text-muted" href="https://www.linkedin.com/company/cartune0/" target="_blank"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+            </ul>
+        </footer>
     </div>
 </body>
 
